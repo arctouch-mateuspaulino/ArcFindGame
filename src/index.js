b3910c7cdf7ae0c16ac6620e8fe089cdb248e080
Wesley Arizio <wesley.arizio@arctouch.com>
2019-09-30 12:53:24 -0300

    add tablet and desktop version, and functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello World')
+
+
+var submitInput = document.querySelector(".input__submit");
+var listItem = document.querySelectorAll(".list__column .item__answer .p__text")
+var counterGameScore = 0;
+var inputs = document.querySelector(".form__input");
+var scoreGame = document.querySelector(".footer__score-game span");
+console.log(scoreGame)
+
+submitInput.addEventListener("click", submit);
+
+function submit(){
+    var validateWrongAnswer = false;
+    var keyword = document.getElementById("input__text").value;
+    listItem.forEach(function(d,i){
+
+        if(keyword.toUpperCase() == listItem[i].innerHTML.toUpperCase()){
+            listItem[i].style.visibility = "visible";
+            counterGameScore++;
+            scoreGame.innerHTML = counterGameScore;
+            validateWrongAnswer= true;
+        }
+
+    })
+    if(validateWrongAnswer == false){
+        inputs.className = "form__input wrong-answer";
+    }  
+    setTimeout(function(){
+        inputs.className = "form__input";
+    }, 1000)
+}
