b52ab7f45697f18ec949949304c8129595c90c7e
Wesley Arizio <wesley.arizio@arctouch.com>
2019-10-03 12:12:43 -0300

    add toltip

--- src/index.js
+++ src/index.js
@@ -13,19 +13,38 @@ function submit(){
     var validateWrongAnswer = false;
     var keyword = document.getElementById("input__text").value;
     listItem.forEach(function(d,i){
-
         if(keyword.toUpperCase() == listItem[i].innerHTML.toUpperCase()){
             listItem[i].style.visibility = "visible";
             counterGameScore++;
             scoreGame.innerHTML = counterGameScore;
             validateWrongAnswer= true;
         }
-
     })
     if(validateWrongAnswer == false){
         inputs.className = "form__input wrong-answer";
     }  
     setTimeout(function(){
         inputs.className = "form__input";
-    }, 1000)
+    }, 1000);
+    startTimer();
+}
+
+var flag = true;
+var duration = 300;
+function startTimer(){
+    if(flag == true){
+    var counter = document.querySelector(".footer__timer-text")
+    var timer = duration, min, sec;
+    setInterval(function(){
+            min = parseInt(timer / 60);
+            sec = parseInt(timer % 60);
+            var timeEnd = `${min}:${sec}`;
+            counter.innerHTML = timeEnd;
+            timer--;
+            if(timer < 0){
+                counter.innerHTML = "The time is over";     
+            }
+        }, 1000);
+        flag = false;
+    }
 }
