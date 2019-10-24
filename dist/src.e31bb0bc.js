45da2b3336d24b7d3c4bafcc25d4522caae9e330
Wesley Arizio <wesley.arizio@arctouch.com>
2019-10-24 11:11:07 -0300

    fixed the duplicate score

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -132,6 +132,7 @@ var timeEnd;
 var timer = duration,
     min,
     sec;
+var wordsList = [];
 submitInput.addEventListener("click", submit);
 
 document.onkeyup = function (e) {
@@ -145,17 +146,10 @@ function submit() {
   var keyword = document.getElementById("input__text").value;
   listItem.forEach(function (d, i) {
     if (keyword.toUpperCase() == listItem[i].innerHTML.toUpperCase()) {
-      listItem[i].style.visibility = "visible";
-      counterGameScore++;
-      scoreGame.innerHTML = counterGameScore;
-      validateWrongAnswer = true;
-
-      if (counterGameScore == 20) {
-        showModal();
-        clearInterval(count);
-      }
+      validateWrongAnswer = verifyWordList(listItem[i]);
     }
   });
+  debugger;
 
   if (validateWrongAnswer == false) {
     inputs.className = "form__input wrong-answer";
@@ -166,6 +160,27 @@ function submit() {
   }, 1000);
   startTimer();
   CleanInput();
+}
+
+function verifyWordList(words) {
+  var isValidWord = null;
+
+  if (!wordsList.includes(words)) {
+    words.style.visibility = "visible";
+    counterGameScore++;
+    scoreGame.innerHTML = counterGameScore;
+    wordsList.push(words);
+    isValidWord = true;
+
+    if (counterGameScore == 20) {
+      showModal();
+      clearInterval(count);
+    }
+  } else {
+    isValidWord = false;
+  }
+
+  return isValidWord;
 }
 
 function startTimer() {
@@ -239,7 +254,7 @@ function CleanInput() {
 
 function setMessage(score) {
   if (score <= 5) {
-    message.innerHTML = "You do con better";
+    message.innerHTML = "You can do better";
   } else if (score > 5 && score <= 10) {
     message.innerHTML = "You are getting better, try again.";
   } else if (score > 10 && score <= 15) {
@@ -278,7 +293,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49520" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49570" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ var duration = 299;
 var count;
 var timeEnd;
 var timer = duration, min, sec;
+var wordsList = [];
 
 submitInput.addEventListener("click", submit);
 document.onkeyup=function(e){
@@ -23,16 +24,10 @@ function submit(){
     var keyword = document.getElementById("input__text").value;
     listItem.forEach(function(d,i){
         if(keyword.toUpperCase() == listItem[i].innerHTML.toUpperCase()){
-            listItem[i].style.visibility = "visible";
-            counterGameScore++;
-            scoreGame.innerHTML = counterGameScore;
-            validateWrongAnswer = true;
-                if(counterGameScore == 20){
-                    showModal();
-                    clearInterval(count);
-                }
-            }
+            validateWrongAnswer = verifyWordList(listItem[i]);   
+        }
     })
+        debugger
         if(validateWrongAnswer == false){
             inputs.className = "form__input wrong-answer";
         }  
@@ -43,6 +38,24 @@ function submit(){
     CleanInput()
 }
 
+function verifyWordList(words){
+    var isValidWord = null; 
+
+    if(!wordsList.includes(words)){
+        words.style.visibility = "visible";
+        counterGameScore++;
+        scoreGame.innerHTML = counterGameScore;
+        wordsList.push(words);
+        isValidWord = true
+           if(counterGameScore == 20){
+               showModal();
+               clearInterval(count);
+           }
+    }else{
+       isValidWord = false 
+    }
+    return isValidWord
+}
 
 function startTimer(){
   if(flag == true){
@@ -113,7 +126,7 @@ function CleanInput(){
 
 function setMessage(score){
     if(score <= 5){
-        message.innerHTML = "You do con better";
+        message.innerHTML = "You can do better";
     }else if(score > 5 && score <=10){
         message.innerHTML = "You are getting better, try again.";
     }else if(score >10 && score <=15){
